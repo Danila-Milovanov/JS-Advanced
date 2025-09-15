48298b53d8eaba710b938145ff8455601aa583f3
Danila-Milovanov <9019853@student.zadkine.nl>
2025-09-15 13:45:22 +0200

    level1

---
+++ 1.2-map-oefening/app.js
@@ -0,0 +1,11 @@
+// Opdracht 1.2: Map Oefening
+console.log('🚀Opdracht 1.2: Map Oefening');
+
+const namen = ['sanne', 'johan', 'sjoerd'];
+console.log('Originele namen:', namen);
+
+// TODO: Zet alle namen om naar hoofdletter
+const hoofdletterNamen = namen.map(naam => naam.charAt(0).toUpperCase() + naam.slice(1));
+
+console.log('Namen met hoofdletter:', hoofdletterNamen);
+console.log('Verwacht resultaat: [\'Sanne\', \'Johan\', \'Sjoerd\']');
---
+++ 1.3-includes-oefening/app.js
@@ -0,0 +1,13 @@
+// Opdracht 1.3: Includes Oefening
+console.log('🚀 Opdracht 1.3: Includes Oefening');
+
+const kleuren = ['rood', 'blauw', 'groen', 'geel'];
+console.log('Beschikbare kleuren:', kleuren);
+
+const zoekKleur ='blauw';
+console.log('Gezochte kleur:', zoekKleur);
+
+// ✅ Check of de kleur in de array staat
+const gevonden = kleuren.includes(zoekKleur);
+
+console.log('Kleur gevonden:', gevonden);
