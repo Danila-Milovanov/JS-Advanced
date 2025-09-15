ab26875312fe996fd317981df69b878c58df491f
Danila-Milovanov <9019853@student.zadkine.nl>
2025-09-15 14:33:18 +0200

    2.1+2.2

---
+++ 2.1-filter-en-map/app.js
@@ -0,0 +1,17 @@
+// Opdracht 2.1 Filter en Map
+console.log('🚀 Opdracht 2.1: Filter en Map');
+
+const prijzen = [800, 25, 75, 300, 150, 45];
+const productnamen = ['laptop', 'muis', 'toetsenbord', 'monitor', 'webcam', 'kabel'];
+
+console.log('Alle prijzen:', prijzen);
+console.log('Alle productnamen:', productnamen);
+
+// TODO: Filter prijzen onder de €100
+const goedkopePrijzen = prijzen.filter(prijs => prijs < 100);
+console.log('Prijzen onder €100:', goedkopePrijzen);
+
+// TODO: Filter productnamen  die beginnen met 'm'
+const productenMetM = productnamen.filter(productnamen => productnamen.toLowerCase().startsWith('m'));
+console.log('Producten die beginnen met m:', productenMetM);
+
---
+++ 2.2-sort-oefening/app.js
@@ -0,0 +1,21 @@
+// Opdracht 2.2 Sort Oefening
+console.log('🚀 Opdracht 2.2: Sort Oefening');
+
+const scores = [85, 92, 76, 88, 95, 73, 89];
+const steden = ['Amsterdam', 'Utrecht', 'Rotterdam', 'Den Haag', 'Eindhoven'];
+
+console.log('Originele scores:', scores);
+console.log('Originele scores:', steden);
+
+// TODO: Sorteer scores van laag naar hoog
+const gesorteerdeScores = scores.sort((a, b) => a - b);
+console.log('Scores laag naar hoog:', gesorteerdeScores);
+
+// TODO: Sorteer scores van hoog naar laag
+const scoresHoogNaarLaag = scores.sort((a, b) => a - b);
+console.log('Scores hoog naar laag:', scoresHoogNaarLaag);
+
+// TODO: Sorteer steden alfabetisch
+const gesorteerdeSteden = steden.sort();
+console.log('Steden alfabetisch:', gesorteerdeSteden);
+
